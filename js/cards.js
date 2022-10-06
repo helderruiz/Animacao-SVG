7f7a9ad6c1171ec323e5c12b6a940949c72d5b5d
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2022-10-06 17:46:51 -0300

    criando eventos com js
    criando animações nos cards

---
+++ js/cards.js
@@ -0,0 +1,9 @@
+class cards{
+    constructor() {
+
+    }
+
+    
+}
+
+export { cards}
---
+++ js/script.js
@@ -0,0 +1,6 @@
+import { cards } from './cards.js'
+
+window.onload = () => {
+    let animacards = new cards()
+    document.addEventListener('scroll', animacards.)
+}
